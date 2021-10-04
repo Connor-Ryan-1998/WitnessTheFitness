b582e9dc34062f029d2d8648ab5cc93525c933af
Connor Ryan <Work1998@hotmail.co.uk>
2021-10-04 18:19:03 +1000

    Update title page

--- Application/pages/index.js
+++ Application/pages/index.js
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Witnesss the Fitness Application</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Witness the Fitness 2.0 <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
