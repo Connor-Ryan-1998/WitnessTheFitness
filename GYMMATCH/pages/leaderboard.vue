17de8dcd3ed1b678ad4a33fad5c0c4ab2439dc6d
Connor Ryan <Work1998@hotmail.co.uk>
2021-10-17 15:36:16 +1000

    Finishing up initial leaderboard

--- GYMMATCH/pages/leaderboard.vue
+++ GYMMATCH/pages/leaderboard.vue
@@ -1,70 +1,120 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <v-card class="pa-md-6 mx-lg-auto"  id="leaderboardHeaderCard" style="background-color: #E94057;">
+      <v-card class="pa-md-6 mx-lg-auto"  id="leaderboardHeaderCard">
         <v-card-title class="justify-left">
-          <h3 class="text-lg-center" id="leaderboardTitle" style="color: white;">Leaderboard</h3>
+          <h3 class="text-lg-center" id="leaderboardTitle">Leaderboard: This week</h3>
         </v-card-title>
       </v-card>
       <v-card class="pa-md-6 mx-lg-auto">
-        <v-carousel v-model="model" class="py-md-6 my-3">
-          <v-carousel-item
-            v-for="(color, i) in colors"
-            :key="color"
-          >
-            <v-sheet
-              :color="color"
-              height="100%"
-              tile
+        <v-row id="leaderboardTop3">
+          <v-col justify="center" align="center">
+            <v-row justify="center" align="center"  style="padding-bottom: 5%;">
+              <v-badge
+              bordered
+              overlap
+              bottom
+              content="2"
+              color="grey"
+              offset-x="50">
+                <v-avatar style = "border: 3px solid silver;"
+                  size="80"
+                  rounded = "true">
+                 <img
+                  src="https://images.freeimages.com/images/large-previews/e71/frog-1371919.jpg"
+                  alt="JF"
+                 >
+                </v-avatar>
+              </v-badge>
+            </v-row>
+            <v-row justify="center" align="center" style="font-family: Arial;font-size: 12pt;color:#9F9F9F;">
+              Julia Frydenburg
+            </v-row>
+            <v-row justify="center" align="center" style="font-family: Arial;font-size: 15pt;color:#17181A;font-weight: bold;">
+              90
+            </v-row>
+          </v-col >
+          <v-col justify="center" align="center">
+            <v-row justify="center" align="center"  style="padding-bottom: 5%;">
+              <v-badge
+              bordered
+              overlap
+              bottom
+              content="1"
+              color="yellow"
+              offset-x="60">
+                <v-avatar style = "border: 3px solid gold;"
+                  size="100"
+                  rounded = "true">
+                  <img
+                  src="https://images.freeimages.com/images/large-previews/70f/clouds-1371112.jpg"
+                  alt="JF"
+                 >
+                </v-avatar>
+              </v-badge>
+            </v-row>
+            <v-row justify="center" align="center" style="font-family: Arial;font-size: 12pt;color:#9F9F9F;">
+              Scott Morrison
+            </v-row>
+            <v-row justify="center" align="center" style="font-family: Arial;font-size: 15pt;color:#17181A;font-weight: bold;">
+              100
+            </v-row>
+          </v-col>
+          <v-col justify="center" align="center">
+            <v-row justify="center" align="center"  style="padding-bottom: 5%;">
+              <v-badge
+              bordered
+              overlap
+              bottom
+              content="3"
+              color="brown"
+              offset-x="50">
+                <v-avatar style = "border: 3px solid brown;"
+                  size="80"
+                  rounded = "true">
+                 <img
+                  src="https://cdn.vuetifyjs.com/images/john.jpg"
+                  alt="JF"
+                 >
+                </v-avatar>
+              </v-badge>
+            </v-row>
+            <v-row justify="center" align="center" style="font-family: Arial;font-size: 12pt;color:#9F9F9F;">
+              Kevin Rudd
+            </v-row>
+            <v-row justify="center" align="center" style="font-family: Arial;font-size: 15pt;color:#17181A;font-weight: bold;">
+              70
+            </v-row>
+          </v-col>
+        </v-row>
+        <v-list-item v-for="(name, i) in top10users" :key="name">
+          <v-badge 
+              bordered
+              overlap
+              left
+              :content = "i + 4"
+              color="#E94057"
+              offset-y="60">
+            <v-list-item-avatar 
+              size="80"
+              color="grey"
+              rounded = "true"
             >
-              <v-row
-                class="fill-height"
-                align="center"
-                justify="center"
-              >
-                <div class="text-h2">
-                  Ranking {{ i + 1 }}
-                </div>
-              </v-row>
-            </v-sheet>
-          </v-carousel-item>
-        </v-carousel>
-      </v-card>
-      <v-card class="pa-md-6 mx-lg-auto">
-          <v-card
-            class="pa-md-3 mx-auto my-4"
-            max-width="550"
-            outlined
-            :style="'border: 1px solid red;'"
-          >
-            <v-list-item v-for="name in top10users" :key="name">
-              <v-list-item-content>
-                <v-list-item-title class="text-h5 mb-1" to="/">
-                  {{ name }}
-                </v-list-item-title>
-                <v-list-item-subtitle>PUNCH HARDER</v-list-item-subtitle>
-              </v-list-item-content>
-
-              <v-list-item-avatar
-                tile
-                size="80"
-                color="grey"
-              ></v-list-item-avatar>
-            </v-list-item>
-
-            <v-card-actions>
-              <v-btn
-                outlined
-                rounded
-                text
-                color="red"
-                nuxt
-                to="/"
-              >
-                VIEW MORE
-              </v-btn>
-            </v-card-actions>
-          </v-card>
+            <v-img v-for="(url) in images" :key="url"
+            :src="url"
+            ></v-img>
+            </v-list-item-avatar>
+          </v-badge>
+          <v-list-item-content>
+            <v-list-item-title class="text-h5 mb-1" to="/">
+              {{ name }}
+            </v-list-item-title>
+            <v-list-item-subtitle v-if="i == 0" style="font-family: Arial;font-size: 12pt;color:#000000;opacity: .5;">Your current rank</v-list-item-subtitle>
+          </v-list-item-content>
+           <v-col justify="right" align="right" style="font-family: Arial;font-size: 15pt;color:#E94057;">
+             {{69 - Math.floor(Math.random() + i)}}
+           </v-col>
+        </v-list-item>
       </v-card>
     </v-col>
   </v-row>
@@ -73,28 +123,36 @@
 
 <script>
   export default {
-      css: [
-        // CSS file in the project
-        '@/assets/leaderboard.scss',
-      ],
     data () {
       return {
-        colors: [
-          'orange',
-          'orange',
-          'orange',
-          'orange',
-          'orange'
-        ],
         top10users: [
           'Monica Geller',
           'Julian Dasilvia',
           'Mike Lyne',
           'Claire Kumas',
-          'Blair Dota'
+          'Blair Dota',
+          'Connor Ryan',
+          'Dendi Navidad',
         ],
         model: 0,
+        images: [
+          'https://picsum.photos/600/200',
+          'https://picsum.photos/300/200',
+          'https://picsum.photos/500/200'
+        ],
       }
     },
   }
 </script>
+
+<style scoped>
+  #leaderboardHeaderCard {
+    background-color: #E94057;
+  }
+  #leaderboardTitle{
+    color: white;
+  }
+  #leaderboardTop3{
+    padding: 10%;
+  }
+</style>
